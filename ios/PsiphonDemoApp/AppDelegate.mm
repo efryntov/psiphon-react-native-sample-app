fc9257425dcfe2191d915130863d7ba6c61f1aee
Eugene Fryntov <e.fryntov@psiphon.ca>
2025-01-28 10:57:34 -0500

    [iOS] fix: ensure bridge is set after super call in didFinishLaunchingWithOptions
    Previously the bridge was being set before the super call, which could result in an uninitialized bridge reference. Now we store the result, get the bridge after super, and then set it on the delegate.

--- ios/PsiphonDemoApp/AppDelegate.mm
+++ ios/PsiphonDemoApp/AppDelegate.mm
@@ -9,31 +9,35 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"PsiphonDemoApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+    self.moduleName = @"PsiphonDemoApp";
+    // You can add your custom initial props in the dictionary below.
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = @{};
 
-  RCTSetCustomNSURLSessionConfigurationProvider(^NSURLSessionConfiguration *{
-    return PsiphonTunnelDelegate.shared.getURLSessionConfiguration;
-  });
+    RCTSetCustomNSURLSessionConfigurationProvider(^NSURLSessionConfiguration *{
+        return PsiphonTunnelDelegate.shared.getURLSessionConfiguration;
+    });
 
-  [PsiphonTunnelDelegate.shared setBridge:self.bridge];
+    BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    RCTBridge *bridge = self.bridge;
+
+    [PsiphonTunnelDelegate.shared setBridge:bridge];
+
+    return result;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+    return [self getBundleURL];
 }
 
 - (NSURL *)getBundleURL
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
